f6e06063c932f94e026a942c845a0167c86009b3
Peter Travers <traverspw@gmail.com>
2025-07-30 19:09:58 -0400

    fix merge issue

--- src/testdrive/src/action/sql_server/cdc.rs
+++ src/testdrive/src/action/sql_server/cdc.rs
@@ -60,8 +60,6 @@ pub async fn run_cdc_scan(
     let timer = Instant::now();
     static CDC_SCAN: &str = "EXEC sys.sp_cdc_scan @continuous = 0, @maxscans = 1;";
     while let Err(error) = client.execute(CDC_SCAN, &[]).await {
-        };
-
         // retry if there is another CDC scan going on
         // 22903 = another connection is already running sp_replcmds
         // see https://learn.microsoft.com/en-us/sql/relational-databases/errors-events/database-engine-events-and-errors-22000-to-22999?view=sql-server-ver17#errors-and-events-22000-to-22999
